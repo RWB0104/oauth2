350f6824dcc93caf8c71a939f403fe1a071e26bf
RWB0104 <psj2716@gmail.com>
2025-10-27 01:53:44 +0900

    feat: ✨ `/callback` 페이지 컴포넌트 신규 추가

---
+++ apps/root/src/pages/callback/+Page.tsx
@@ -0,0 +1,48 @@
+/**
+ * 콜백 페이지 컴포넌트
+ *
+ * @author RWB
+ * @since 2025.10.05 Sun 23:41:09
+ */
+
+import { useCallback, useEffect } from 'react';
+import { usePageContext } from 'vike-react/usePageContext';
+
+/**
+ * 콜백 페이지 컴포넌트 반환 메서드
+ *
+ * @returns {React.JSX.Element} JSX
+ */
+export default function CallbackPage(): React.JSX.Element {
+	const {
+		urlParsed: { search }
+	} = usePageContext();
+
+	const getMeApi = useCallback(async (token: string) => {
+		const response = await fetch('http://localhost:8080/api/me', {
+			headers: {
+				// biome-ignore lint/style/useNamingConvention: just header name
+				Authorization: `Bearer ${token}`
+			},
+			method: 'GET'
+		});
+
+		if (response.ok) {
+			const json = await response.json();
+
+			// biome-ignore lint/suspicious/noConsole: for test
+			console.log(json);
+		}
+	}, []);
+
+	useEffect(() => {
+		const { token } = search;
+
+		if (token) {
+			// biome-ignore lint/nursery/noFloatingPromises: for test
+			getMeApi(token);
+		}
+	}, [search, getMeApi]);
+
+	return <div>2342</div>;
+}
