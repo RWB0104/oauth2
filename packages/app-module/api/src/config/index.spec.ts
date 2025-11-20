cf5564b3b6a920e7e7653f4bade6d0ae599371ae
RWB0104 <psj2716@gmail.com>
2025-11-21 01:50:05 +0900

    chore: 💡 주석 누락 추가

--- packages/app-module/api/src/config/index.spec.ts
+++ packages/app-module/api/src/config/index.spec.ts
@@ -1,3 +1,10 @@
+/**
+ * 설정 인덱스 테스트 모듈
+ *
+ * @author RWB
+ * @since 2025.11.14 Fri 22:33:12
+ */
+
 import { getConfig, getConfigItem } from '.';
 
 describe('[@oauth2/api] 설정 인덱스 테스트 모듈', () => {
