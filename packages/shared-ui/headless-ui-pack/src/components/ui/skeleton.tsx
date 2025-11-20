8b93d30653bd7c5a61d6754305a5b8dec58f1483
RWB0104 <psj2716@gmail.com>
2025-11-21 01:37:46 +0900

    feat: ✨ `shadcn` 컴포넌트 신규 추가
    - `Alert`
    - `Badge`
    - `Card`
    - `Skeleton`

---
+++ packages/shared-ui/headless-ui-pack/src/components/ui/skeleton.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@oauth2/headless-ui-pack/lib/utils"
+import * as React from "react"
+
+function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="skeleton"
+      className={cn("bg-accent animate-pulse rounded-md", className)}
+      {...props}
+    />
+  )
+}
+
+export { Skeleton }
